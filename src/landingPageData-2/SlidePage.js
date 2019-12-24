be97b54047bb897778a3bd7e9d71b11b728cf278
Jui-Jen Chang <raychang1125@gmail.com>
2019-12-24 20:40:31 +0800

    update191224

--- src/landingPageData-2/SlidePage.js
+++ src/landingPageData-2/SlidePage.js
@@ -2,14 +2,53 @@ import React, { Component } from 'react';
 import './SlidePage.css';
 
 class SlidePage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            leftHover: false,
+            rightHover: false,
+        }
+    }
+
+    onMouseEnterLeft = () => {
+        this.setState({
+            rightHover: true,
+        })
+    }
+
+    onMouseLeaveLeft = () => {
+        this.setState({
+            rightHover: false,
+        })
+    }
+
+    onMouseEnterRight = () => {
+        this.setState({
+            leftHover: true,
+        })
+    }
+
+    onMouseLeaveRight = () => {
+        this.setState({
+            leftHover: false,
+        })
+    }
+    
     render() {
         return (
             <div>
-                <div className="sp-left" >
+                <div className="sp-left"
+                    onMouseEnter={this.onMouseEnterLeft} onMouseLeave={this.onMouseLeaveLeft}
+                    style={this.state.leftHover? {width: '15%'} : {width: '50%'} }
+                >
                     <h1>CG Generalist</h1>
                     <a href="#" >LET'S GO</a>
                 </div>
-                <div className="sp-right" >
+                <div className="sp-right"
+                    onMouseEnter={this.onMouseEnterRight} onMouseLeave={this.onMouseLeaveRight}
+                    style={this.state.rightHover? {width: '15%'} : {width: '50%'} }
+                >
                     <h1>Designer/Developer</h1>
                     <a href="#" >LET'S GO</a>
                 </div>
